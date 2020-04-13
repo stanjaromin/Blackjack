7787350911ade60b6a4fb5b4461470f2b8e995b2
Stanislaw Jaromin <jaromin@onet.pl>
2020-04-13 22:06:36 +0200

    First commint

---
+++ blackjack.py
@@ -0,0 +1,12 @@
+import random
+
+try:
+    import tkinter
+except ImportError:
+    import TKinter as tkinter
+
+mainWindow = tkinter.Tk()
+
+mainWindow.title("Black Jack")
+mainWindow.geometry("640x480")
+
