d83e030e723143e428bdee0f103d93262d8904a5
Stanislaw Jaromin <jaromin@onet.pl>
2020-04-14 21:34:33 +0200

    First commint

--- blackjack.py
+++ blackjack.py
@@ -7,6 +7,28 @@ except ImportError:
 
 mainWindow = tkinter.Tk()
 
+
+def load_images(card_images):
+    suits=['heart', 'club', 'diamond', 'spade']
+    face_cards=['jack', 'queen', 'king']
+
+    if tkinter.TkVersion >= 8.6:
+        extension = 'png'
+    else:
+        extension = 'ppm'
+
+    for suit in suits:
+        for card in range (1,11):
+            name = 'cards/{}_{}.{}'.format(str(card), suit, extension)
+            image = tkinter.PhotoImage(file=name)
+            card_images.append((card, image,))
+
+        for card in face_cards:
+            name = 'cards/{}_{}.{}'.format(str(card), suit, extension)
+            image = tkinter.PhotoImage(file=name)
+            card_images.append((10, image,))
+
+
 mainWindow.title("Black Jack")
 mainWindow.geometry("640x480")
 
@@ -14,8 +36,8 @@ result_text = tkinter.StringVar()
 result = tkinter.Label(mainWindow, textvariable=result_text)
 result.grid(row=0, column=0, columnspan=3)
 
-card_frame = tkinter.Frame(mainWindow, relief="sunker", borderwidth=1, background="green")
-card_frame.grid(row=1, column=0, sticky='ew', columnspan=3, rowspan=2)
+card_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth=1, background="green")
+card_frame.grid(row=1, column=0, sticky="ew", columnspan=3, rowspan=2)
 
 dealer_score_label = tkinter.IntVar()
 tkinter.Label(card_frame, text="Dealer", background="green", fg="white").grid(row=0, column=0)
